f90a23a84056fb1a68df22bbb4bdfda95a653ca4
dovadallas661-ux <dovadallas661@gmail.com>
2025-10-18 18:39:05 +0100

    Implement event listeners and answer validation

--- Script.js
+++ Script.js
@@ -43,12 +43,29 @@
     // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
             // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
 
+     for (let i = 0; i < optionButtons.length; i++) {
+     optionButtons[i].addEventListener("click", function () {
+     console.log("You clicked:", optionButtons[i].textContent);
+     });
+     }
 
             // TODO 7: Within the event handler function, 
             // Use a for loop to disable all the option buttons
 
+              for (let i = 0; i < optionButtons.length; i++) {
+              optionButtons[i].disabled = true;
+              }
 
             // TODO 8: Within the event handler function,
             // Get the guessed value from the clicked button
             // Use a conditional to compare the guess to the fact's answer
+
+             if (guessedValue === currentFact.answer) {
+               event.target.classList.add('correct');
+                } else {
+               event.target.classList.add('incorrect');
+               }
+               }
+
+
             // and add the "correct"/"incorrect" class as appropriate
